e81871ef5b0b9883bf9d1f3cd06a89c5f0192a37
LiKTOi1029 <testingboy5689@gmail.com>
2025-07-04 04:15:15 +0330

    wordgen.lua saving progress.

--- wordgen.lua
+++ wordgen.lua
@@ -1,8 +1,15 @@
-function SyllablePutTogether(choice)
+math.randomseed(os.clock())
+function RandomValueGenerator(V, C, SV, Weights)
 	
 end
+function SyllablePutTogether(choice)
+	local V, C, SV, Weights = {"a","e","i","o","u"}, {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z",}, "y", {45,45,10}
+	for num1 = 1, choice, 1 do
+		RandomValueGenerator(V, C, SV, Weights)
+	end
+end
 repeat
-	io.write("Write a number to generate syllables and words from those syllables")
+	io.write("Write two numbers, one for the number of words and another for syllables per word.\nUse spaces as the delimiter I.E. \"3 3\"\n type exit to exit")
 	local choice = io.read():gsub("\n","")
-	if choice:upper() ~= "EXIT" and choice == int then SyllablePutTogether(choice) end
+	if choice:match("^%d+%s+%d+$") then SyllablePutTogether(choice) end
 until choice:upper() == "EXIT'"
